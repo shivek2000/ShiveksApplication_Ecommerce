36d9395875d7f1a74b739c944fa427c1b73670f4
Shivek Dhurumpall <shivek.dhurumpall@nologostudios.com>
2023-05-22 11:09:13 +0200

    Add project files.

---
+++ ShiveksApplication_Ecommerce/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ShiveksApplication_Ecommerce.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
